3430771433b1dc4791d10ff5f0b71e4978ea563f
Dmitry <koliasam@yandex.ru>
2020-01-14 10:58:09 +0300

    Adding to DOM labs

--- Chapter2/app.js
+++ Chapter2/app.js
@@ -171,3 +171,274 @@
 /**
  * Задание 2.9
  */
+
+// let uli = document.querySelector('ul');
+
+// const [...children] = uli.children;
+
+// let lengthUl = children.length;
+// let count = 3;
+
+// for (let i = 1; i < count; i++) {
+//     var setCount = lengthUl + i;
+//     var li = document.createElement('li');
+//     li.className = 'new - item';
+//     li.textContent = `item ${setCount}`;
+//     uli.appendChild(li);
+// }
+
+
+
+/**
+ * Задание 2.10
+ */
+
+// let uli = document.querySelector('ul');
+// const [...children] = uli.children;
+
+// for (let i = 0; i < children.length; i++) {
+//     uli.children[i].insertAdjacentHTML('beforeend', '<strong> </strong>');
+// }
+
+
+
+
+/**
+ * Задание 2.11
+ */
+// var img = document.createElement('img');
+// img.setAttribute('src', '#');
+// img.setAttribute('alt', 'Photo is there');
+// document.body.insertBefore(img, document.body.firstChild);
+
+
+
+
+
+/**
+ * Задание 2.12
+ */
+// let mark = document.querySelector('mark');
+// mark.setAttribute('class', 'green');
+// mark.insertAdjacentText('beforeend', ' green ');
+
+
+
+
+/**
+ * Задание 2.13
+ */
+
+// let uli = document.querySelector('ul');
+// const [...children] = uli.children;
+
+// for (let i = children.length - 1; i >= 0; i--) {
+//     let li = uli.children[i];
+//     console.log(li);
+//     li.insertAdjacentElement('afterend', uli.children[0]);
+// }
+// console.log(uli);
+
+
+
+
+/**
+ * Задание 2.14
+ */
+
+// const map = {
+//     id: "ID",
+//     name: "Name",
+//     email: "Email",
+//     balance: "Balance"
+// };
+// const users = [{
+//         "_id": "5d220b10e8265cc978e2586b",
+//         "isActive": true,
+//         "balance": 2853.33,
+//         "age": 20,
+//         "name": "Buckner Osborne",
+//         "gender": "male",
+//         "company": "EMPIRICA",
+//         "email": "bucknerosborne@empirica.com",
+//         "phone": "+1 (850) 411-2997",
+//         "registered": "2018-08-13T04:28:45 -03:00",
+//         "nestedField": { total: 300 }
+//     },
+//     {
+//         "_id": "5d220b10144ef972f6c2b332",
+//         "isActive": true,
+//         "balance": 1464.63,
+//         "age": 38,
+//         "name": "Rosalie Smith",
+//         "gender": "female",
+//         "company": "KATAKANA",
+//         "email": "rosaliesmith@katakana.com",
+//         "phone": "+1 (943) 463-2496",
+//         "registered": "2016-12-09T05:15:34 -02:00",
+//         "nestedField": { total: 400 }
+//     },
+//     {
+//         "_id": "5d220b1083a0494655cdecf6",
+//         "isActive": false,
+//         "balance": 2823.39,
+//         "age": 40,
+//         "name": "Estrada Davenport",
+//         "gender": "male",
+//         "company": "EBIDCO",
+//         "email": "estradadavenport@ebidco.com",
+//         "phone": "+1 (890) 461-2088",
+//         "registered": "2016-03-04T03:36:38 -02:00",
+//         "nestedField": { total: 200 }
+//     }
+// ];
+
+
+// var fragment = document.createDocumentFragment();
+// var table = document.createElement('table');
+// var tr = document.createElement('tr');
+// table.insertAdjacentElement('beforeend', tr);
+
+
+// for (let i in map) {
+//     var th = document.createElement('th');
+//     th.insertAdjacentText('beforeend', map[i]);
+//     tr.insertAdjacentElement('beforeend', th);
+// }
+
+// let totalPrice = 0;
+
+// for (let j in users) {
+//     var tr1 = document.createElement('tr');
+
+//     var th4 = document.createElement('td');
+//     th4.insertAdjacentText('beforeend', j);
+
+//     var th1 = document.createElement('td');
+//     th1.insertAdjacentText('beforeend', users[j].name);
+
+//     var th2 = document.createElement('td');
+//     th2.insertAdjacentText('beforeend', users[j].email);
+
+//     var th3 = document.createElement('td');
+//     th3.insertAdjacentText('beforeend', users[j].balance);
+
+//     tr1.insertAdjacentElement('beforeend', th4);
+//     tr1.insertAdjacentElement('beforeend', th1);
+//     tr1.insertAdjacentElement('beforeend', th2);
+//     tr1.insertAdjacentElement('beforeend', th3);
+
+//     table.insertAdjacentElement('beforeend', tr1);
+//     totalPrice += users[j].balance;
+// }
+
+// let tr_total = document.createElement('tr');
+
+// let th4_total = document.createElement('th');
+// th4_total.insertAdjacentText('beforeend', " ");
+// tr_total.insertAdjacentElement('beforeend', th4_total);
+
+// let th1_total = document.createElement('th');
+// th1_total.insertAdjacentText('beforeend', " ");
+// tr_total.insertAdjacentElement('beforeend', th1_total);
+
+// let th2_total = document.createElement('th');
+// th2_total.insertAdjacentText('beforeend', " ");
+// tr_total.insertAdjacentElement('beforeend', th2_total);
+
+// let th3_total = document.createElement('th');
+// th3_total.insertAdjacentText('beforeend', totalPrice);
+// tr_total.insertAdjacentElement('beforeend', th3_total);
+
+// table.insertAdjacentElement('beforeend', tr_total);
+
+// fragment.appendChild(table);
+// document.querySelector('.table').appendChild(fragment);
+
+
+
+
+
+
+
+
+/**
+ * Задание 3.1
+ */
+// let btn_msg = document.querySelector('#btn-msg');
+// btn_msg.addEventListener('click', e => {
+//     alert(btn_msg.getAttribute('data-text'));
+// });
+
+
+
+/**
+ * Задание 3.2
+ */
+
+// let btn_msg = document.querySelector('#btn-msg');
+
+// btn_msg.addEventListener('mouseenter', e => {
+//     btn_msg.setAttribute('style', 'background: #fc0707');
+// });
+
+// btn_msg.addEventListener('mouseleave', e => {
+//     btn_msg.setAttribute('style', ' ');
+// });
+
+
+
+
+/**
+ * Задание 3.3
+ */
+
+// let btn_msg = document.querySelector('#btn-msg');
+// let btn_gen = document.querySelector('#btn-generate');
+
+// btn_msg.addEventListener('click', e => {
+//     document.querySelector('#tag').textContent = btn_msg.getAttribute('id');
+// });
+
+
+// btn_gen.addEventListener('click', e => {
+//     document.querySelector('#tag').textContent = btn_gen.getAttribute('id');
+// });
+
+
+
+
+
+
+
+/**
+ * Задание 3.4
+ */
+
+// let btn_gen = document.querySelector('#btn-generate');
+// let count = 2;
+
+// btn_gen.addEventListener('click', e => {
+//     let li = document.createElement('li');
+//     li.textContent = "Item " + ++count;
+//     document.querySelector('ul').appendChild(li);
+// });
+
+
+
+
+
+/**
+ * Задание 3.5
+ */
+
+let item = document.querySelector('.dropdown-item');
+let none = document.querySelector('.dropdown-menu');
+
+item.addEventListener('click', e => {
+    if (none.getAttribute('stylr') === 'display: list-item') {
+        none.setAttribute('style', 'display: none');
+    } else {
+        none.setAttribute('style', 'display: list-item');
+    }
+})
